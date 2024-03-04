741f4517f7f2eba75c8bf744d075b63463db3360
Elizabeth11 <semartine1@eafit.edu.co>
2024-03-03 22:44:19 -0500

    My Workflow

--- src/model/train.py
+++ src/model/train.py
@@ -4,6 +4,7 @@ import wandb
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 import numpy as np
 
 # Parser para los argumentos de la línea de comandos
@@ -24,12 +25,21 @@ X_train, X_test, y_train, y_test = train_test_split(wbcd.data, wbcd.target, test
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
+# Predictions
+y_pred = model.predict(X_test)
+y_probas = model.predict_proba(X_test)
+labels = np.unique(y_test)
+
 # Log model and evaluation metrics to W&B
-with wandb.init(project="PracticaMLOps", name="RandomForest", id=args.IdExecution):
+with wandb.init(project="breast-cancer-classification", name="RandomForest", id=args.IdExecution):
     # Log model
-    wandb.sklearn.plot_classifier(model, X_train, X_test, y_train, y_test, model_name="RandomForest")
+    wandb.sklearn.plot_classifier(model, X_train, X_test, y_train, y_test, y_pred, y_probas, labels, model_name="RandomForest")
 
     # Log evaluation metrics
-    y_pred = model.predict(X_test)
     accuracy = np.mean(y_pred == y_test)
     wandb.log({"accuracy": accuracy})
+
+    # Classification report
+    report = classification_report(y_test, y_pred, target_names=['Malignant', 'Benign'], output_dict=True)
+    wandb.log({"classification_report": report})
+
